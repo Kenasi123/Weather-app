b16caa943933eaf422f4ba49940592d1eab489b7
KenasiaTurner <kenasi123@gmail.com>
2022-04-14 22:35:02 -0400

    adds changes to display temp

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
 document.addEventListener("DOMContentLoaded", function() {
 
+
         // get the forcast and the elements from the forecast
-        function getForcast () {
+    function getForcast () {
         const API_KEY = "a35c94fe128a46eaa9e174739221204" // has to hidden and imported 
         
             fetch(`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=London&days=7&aqi=no&alerts=no`).then(function(response) {
@@ -11,19 +12,29 @@ document.addEventListener("DOMContentLoaded", function() {
                }
                 return response.json();
             }).then(function(data) {
-               
+
+                //get the location
+
                 // weather.temperature = data.current.temp_c;
                 
                 const temperature = Math.round(data.current.temp_c);
-                const conditions = data.current.condition.text;
-                const iconId = data.current.condition.icon;
-                const high= data.forecast.forecastday.maxtemp_c;
-                const low = data.forecast.forecastday.mintemp_c;
+                const conditions = (data.current.condition.text);
+                const iconId = (data.current.condition.icon);
+                const high = Math.round(data.forecast.forecastday.maxtemp_c);
+                const low = Math.round(data.forecast.forecastday.mintemp_c);
 
-                //document.getElementById("city").innerHTML = body.main.temp + "degrees";
+                
+                document.getElementById("temp").innerHTML = `${temperature} °C`;
+                document.getElementById("conditons").innerHTML = `${conditions}`;
+                document.getElementById("H").innerHTML = `High: ${high} °C`;
+                document.getElementById("L").innerHTML = `Low: ${low} °C`;
+                //document.getElementById("city").innerHTML = location; 
+
+
+                // get the icon
 
                 //Daily Forecast 
-                let nextDay = Math.round(data.daily[0].temp.day); 
+                let nextDay = data.hour.time; 
 
             }).catch(function (error) {
                 console.log("Theres a error!")
