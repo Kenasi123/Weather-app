aff128d3fcd5162196f4e9c067f300b9390f96f2
KenasiaTurner <kenasi123@gmail.com>
2022-04-15 00:49:13 -0400

    adds functionality for current day

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ document.addEventListener("DOMContentLoaded", function() {
         // get the forcast and the elements from the forecast
     function getForcast () {
         const API_KEY = "a35c94fe128a46eaa9e174739221204" // has to hidden and imported 
+
+        let city = "Atlanta"
         
-            fetch(`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=London&days=7&aqi=no&alerts=no`).then(function(response) {
+            fetch(`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=7&aqi=no&alerts=no`).then(function(response) {
                if (response.status >= 400 ) {
                    console.log("Theres a error")
                }
@@ -15,26 +17,31 @@ document.addEventListener("DOMContentLoaded", function() {
 
                 //get the location
 
-                // weather.temperature = data.current.temp_c;
+    
                 
-                const temperature = Math.round(data.current.temp_c);
-                const conditions = (data.current.condition.text);
-                const iconId = (data.current.condition.icon);
-                const high = Math.round(data.forecast.forecastday.maxtemp_c);
-                const low = Math.round(data.forecast.forecastday.mintemp_c);
+            const temperature = Math.round(data.current.temp_c);
+            const conditions = data.current.condition.text;
+            const iconId = data.current.condition.icon;
+            const high = Math.round(data.forecast.forecastday[0].day.maxtemp_c);
+            const low = Math.round(data.forecast.forecastday[0].day.mintemp_c);
 
+               
+            document.getElementById("temp").innerHTML = `${temperature} °C`;
+            document.getElementById("conditions").innerHTML = `${conditions}`;
+            document.getElementById("H").innerHTML = `${high} °C`;
+            document.getElementById("L").innerHTML = `${low} °C`;
+            //document.getElementById("city").innerHTML = location; 
+
+
+            // get the icon
+            var img = document.createElement("img");
+            img.src = `https:${iconId}`;
+            var src = document.getElementById("icon");
+            src.appendChild(img);
                 
-                document.getElementById("temp").innerHTML = `${temperature} °C`;
-                document.getElementById("conditons").innerHTML = `${conditions}`;
-                document.getElementById("H").innerHTML = `High: ${high} °C`;
-                document.getElementById("L").innerHTML = `Low: ${low} °C`;
-                //document.getElementById("city").innerHTML = location; 
-
-
-                // get the icon
-
-                //Daily Forecast 
-                let nextDay = data.hour.time; 
+                
+            //Weekly Forecast 
+            
 
             }).catch(function (error) {
                 console.log("Theres a error!")
