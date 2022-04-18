fff3142fcbd8a93fbf07129e95e967cc7fe41a1e
KenasiaTurner <kenasi123@gmail.com>
2022-04-17 20:40:29 -0400

    adds the advice api

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-const d = new Date("03/25/2015"); 
+document.addEventListener("DOMContentLoaded", function() {
+ 
+const resDiv = document.getElementById("affirmations");
+
+// adds advice couldnt get the affirmations to work 
+ 
+    function getAdvice() {
+    
+        fetch('https://api.adviceslip.com/advice').then(response => {
+            return response.json();
+        }).then(data => {
+            console.log(data);
+            const Adviceobj = data.slip.advice;
+            resDiv.innerHTML = `<p>${Adviceobj}</p>`;
+        }).catch(error => {
+            console.log(error);
+        });
+        
+    }
+
+    getAdvice();
+
+
+   
+})
